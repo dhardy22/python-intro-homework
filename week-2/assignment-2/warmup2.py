5de893a4065009ff57cbffc2b1b4530b530a3a98
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-11 18:51:14 -0400

    Completed warmup1 w/ my own flair on input command

---
+++ week-2/assignment-2/warmup2.py
@@ -0,0 +1,8 @@
+# Navigation commands I used:
+# cd week-2/assignment-2/
+# touch warmup2.py
+# code warmup2.py
+
+date = input("What is today's date? (Enter month date, year):")
+
+print(f"You said today is {date}")
