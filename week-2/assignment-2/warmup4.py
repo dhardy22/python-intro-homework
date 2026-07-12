0be41bd33e54d9e2c7bb1af316fae73b43dac630
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-12 15:10:31 -0400

    wrote a buggy script for wamrup4 with commented block answering the required questions

---
+++ week-2/assignment-2/warmup4.py
@@ -0,0 +1,16 @@
+name = input("what is your name?:")
+
+print(f"Hi {name}, time flies by fast.")
+age = int(input("How old are you?:"))
+
+add_tenyears = age + 10
+print(f"In 10 years you will be {add_tenyears}.")
+
+
+#1. What the error message said (paste it): Traceback (most recent call last):
+#   File "/Users/donovanhardy/CTD/python_class/python-intro-homework/week-2/assignment-2/warmup4.py", line 6, in <module>
+#     add_tenyears = {age + 10}
+#                     ~~~~^~~~
+# TypeError: can only concatenate str (not "int") to str
+#2. What caused it: since age is a strings by default since it is an input, age, a string, could not be added to 10, an integer 
+#3. How you fixed it: to fix it, i will typecast age to an integer.
