9abe01860fc1482a9297ab18730f5031dbaf0374
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-11 20:45:27 -0400

    Completed warmups 1-3. Thouroughly confused, but pushing through

---
+++ week-2/assignment-2/warmup3.py
@@ -0,0 +1,28 @@
+# feadfb7 (HEAD -> assignment-2) Add warmup1 in assignment-2 folder
+# 5de893a Completed warmup1 w/ my own flair on input command
+# 294742d (origin/main, origin/HEAD, main) add week 2 folder
+# 58d836d Add files via upload
+# d8c0c05 Update README.md
+# a888c4b Delete week-5/data/.gitkeep
+# 06c7f75 Delete week-4/data/.gitkeep
+# 202444e Add files via upload
+# 677fee9 Add files via upload
+# ff7d9dc Add files via upload
+# e0ff870 Delete week-7/data/.gitkeep
+# c3876b0 Add files via upload
+# 30299f8 Merge pull request #1 from Code-the-Dream-School/add-gitkeeps
+# bd6befc foldering
+# 5f7a204 Create .gitkeep
+# d0a5396 Create .gitkeep
+# 545c44d Delete students.csv
+# 19f098e Delete notes.txt
+# 66766d2 Delete expenses.csv
+# a69c42e foldering
+# c0de231 Add files via upload
+# c487413 create folders & update instructions
+# 8b36eac Update README.md
+# 2eaa21f Initial commit
+
+print("This week I learned how to configure Homebrew, git, and GitHub. As well as how to make and clone repositories to my local machine. I have used the command line to make and open .py files and have practiced the core workflow of pull requests, creating branches off main, and making commits")
+
+
