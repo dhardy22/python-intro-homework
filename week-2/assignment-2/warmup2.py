1c59a20560f3ae104b802cbb6bc854de8a0426ac
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-12 19:56:10 -0400

    cosmetic change: added a space after the colon on the input string

--- week-2/assignment-2/warmup2.py
+++ week-2/assignment-2/warmup2.py
@@ -3,6 +3,6 @@
 # touch warmup2.py
 # code warmup2.py
 
-date = input("What is today's date? (Enter month date, year):")
+date = input("What is today's date? (Enter month date, year): ")
 
 print(f"You said today is {date}")
