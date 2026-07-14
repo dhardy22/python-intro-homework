bbcc365a7c51c0c988306a8914dff7888cefc231
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-14 15:54:30 -0400

    tweaked the logic to meet the assignment conditions

---
+++ week-3/warmup1.py
@@ -0,0 +1,12 @@
+score = 75  
+
+if score >= 90 :
+    print(f" Score: {score} Grade: A " )
+elif score <= 89 and score >= 80 :
+    print(f" Score: {score} Grade: B ")
+elif score <= 79 and score >= 70 :
+    print(f" Score: {score} Grade: C ")
+elif score <= 69 and score >= 60 :
+    print(f" Score: {score} Grade: D ")
+else :
+    print(f" Score: {score} Grade: F ")
