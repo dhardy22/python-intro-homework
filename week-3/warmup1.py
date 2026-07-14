45591f86ea6df770816d134cd9ae165372084fb3
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-14 16:07:51 -0400

    added a line break between score and grade

--- week-3/warmup1.py
+++ week-3/warmup1.py
@@ -1,12 +1,12 @@
 score = 75  
 
 if score >= 90 :
-    print(f" Score: {score} Grade: A " )
+    print(f" Score: {score} \n Grade: A " )
 elif score <= 89 and score >= 80 :
-    print(f" Score: {score} Grade: B ")
+    print(f" Score: {score} \n Grade: B ")
 elif score <= 79 and score >= 70 :
-    print(f" Score: {score} Grade: C ")
+    print(f" Score: {score} \n Grade: C ")
 elif score <= 69 and score >= 60 :
-    print(f" Score: {score} Grade: D ")
+    print(f" Score: {score} \n Grade: D ")
 else :
-    print(f" Score: {score} Grade: F ")
+    print(f" Score: {score} \n Grade: F ")
