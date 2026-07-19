be6934b2f45ca3a05f164576edafe4bc0228dd9f
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-19 18:23:53 -0400

    fixed warmup 3 formatting

--- week-3/assignment-3/warmup3.py
+++ week-3/assignment-3/warmup3.py
@@ -1,15 +1,15 @@
+
 result_1 = not True and False
-print(f"not True and False: {result_1}") # Because not True is False. False and False is False because AND requires both statements to be True
+print(f"{result_1} # Because not True is False. False and False is False because AND requires both statements to be True ") 
 
 result_2 = True or False and False
-print(f"True or False and False: {result_2}") # Because AND is evaluated first so False and False is False. OR requires one of the values to be True so it True
+print(f"{result_2} # Because AND is evaluated first so False and False is False. OR only requires one variable to be True")
 
 result_3 = not (5 > 3) 
-print(f"not (5 > 3): {result_3}") # Because 5 is greater than 3 is True. not True is False
+print(f"{result_3} # Because 5 is greater than 3 is True. not True is False")
 
 result_4 = 10 == 10 and 4 != 4 
-print(f"10 == 10 and 4 != 4: {result_4}") # Because 10 is equivalent to 10 is True. 4 not equal to 4 is False. AND requires both statements to be True so it is False
+print(f"{result_4} # Because 10 is equivalent to 10 is True. 4 not equal to 4 is False. AND requires both operands to be True so it is False")
 
 result_5 = not False or not True
-print(f"not False or not True: {result_5}") # Because not False is True. not True is False. For OR only one condition has to be True so it is True
-
+print(f"{result_5} # Because not False is True. not True is False. For OR only one condition has to be True so it is True")
