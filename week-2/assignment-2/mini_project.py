56ae88abbc486421afccd5a4fc89aad3f5761ec1
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-18 23:01:46 -0400

    completed the airhub suggestions

--- week-2/assignment-2/mini_project.py
+++ week-2/assignment-2/mini_project.py
@@ -2,4 +2,4 @@ fahrenheit = float(input("Enter a temperature in Fahrenheit: "))
 
 celsius = (fahrenheit - 32) * 5/9
 
-print(f"{fahrenheit:.1f}°F is {celsius:.1f}°C")
+print(f"{fahrenheit:.1f}°F is {celsius:.1f}°C.")
