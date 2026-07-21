51f4a05c73bc1e92c39e0fc49de9869b39da3a32
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-21 18:19:59 -0400

    added a conditional block to check for valid days and times w/ appropriate msg

--- week-3/assignment-3/mini_project.py
+++ week-3/assignment-3/mini_project.py
@@ -6,40 +6,52 @@
 # Normalize input so capitalization doesn't matter (e.g., "Monday" and "monday" both work)
 
 #User inputs for day and time of day
-day_of_week = input("What day is it?: ").casefold()
-time_of_day = input("What is the time of day?: ").casefold()
+day_of_week = input("What day is it?: ").casefold().strip()
+time_of_day = input("What is the time of day?: ").casefold().strip()
 
+#contains all of the valid days contained in valid days and valid times
+valid_days = ("monday", "friday", "saturday")
+valid_times = ("morning", "afternoon", "evening")
 
+#this block checks for the valid response inside of valid days and valid times
+if day_of_week not in valid_days and time_of_day not in valid_times:
+    print("I don't recognize either your day or your time. Try Monday, Friday, or Saturday; Morning, Afternoon, or Evening.")
+elif day_of_week not in valid_days:
+    print("I don't recognize that day. Try Monday, Friday, or Saturday.")
+elif time_of_day not in valid_times:
+    print("I don't recognize that time. Try Morning, Afternoon, or Evening.")
+else:
+    ...
 #Monday suggestions
-if day_of_week == "monday" and time_of_day == "morning" :
-    print("Enjoy a slow morning - Mondays can get crazy")
+    if day_of_week == "monday" and time_of_day == "morning" :
+        print("Enjoy a slow morning - Mondays can get crazy")
 
-elif day_of_week == "monday" and time_of_day == "afternoon" :
-    print("Refuel with coffee or tea!")
+    elif day_of_week == "monday" and time_of_day == "afternoon" :
+        print("Refuel with coffee or tea!")
 
-elif day_of_week == "monday" and time_of_day == "evening" :
-    print("Relax, You survived another Monday!")
+    elif day_of_week == "monday" and time_of_day == "evening" :
+        print("Relax, You survived another Monday!")
 
-#Friday suggestions
-elif day_of_week == "friday" and time_of_day == "morning" :
-    print("“Hey Siri, Play Friday (Dopamine Re-Edit)”")
+    #Friday suggestions
+    elif day_of_week == "friday" and time_of_day == "morning" :
+        print("“Hey Siri, Play Friday (Dopamine Re-Edit)”")
 
-elif day_of_week == "friday" and time_of_day == "afternoon" :
-    print("Wrap up and log off!")
+    elif day_of_week == "friday" and time_of_day == "afternoon" :
+        print("Wrap up and log off!")
 
-elif day_of_week == "friday" and time_of_day == "evening" :
-    print("The couch is calling you. I’m not judging")
+    elif day_of_week == "friday" and time_of_day == "evening" :
+        print("The couch is calling you. I’m not judging")
 
-#Saturday suggestions
-elif day_of_week == "saturday" and time_of_day == "morning" :
-    print("Get some exercise!")
+    #Saturday suggestions
+    elif day_of_week == "saturday" and time_of_day == "morning" :
+        print("Get some exercise!")
 
-elif day_of_week == "saturday" and time_of_day == "afternoon" :
-    print("Check out the farmers market")
+    elif day_of_week == "saturday" and time_of_day == "afternoon" :
+        print("Check out the farmers market")
 
-elif day_of_week == "saturday" and time_of_day == "evening" :
-    print("Go out on the town")
-else :
-    print("Sorry, I didn't recognize that input. Try entering Monday, Friday, or Saturday; Morning, Afternoon, or Evening!")
+    elif day_of_week == "saturday" and time_of_day == "evening" :
+        print("Go out on the town")
+    else :
+        ...
 
-
+print(repr(day_of_week), repr(time_of_day))
