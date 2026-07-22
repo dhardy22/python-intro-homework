73bb3bf1bcce383b525f4f80e29a587254498618
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-21 21:27:40 -0400

    fixed the zero case logic and added comments to justify the solution

--- week-3/assignment-3/warmup4.py
+++ week-3/assignment-3/warmup4.py
@@ -4,7 +4,8 @@
 
 number = int(input("Enter a number: "))
 
-#conditional block #1 tests even or odd and zero case
+#conditional block #1 number can either be less than or greater than 0 to satisfy one of the first two branches
+#if the number is neither less than nor greater than 0, the number has to be zero
 
 if number < 0 :
     print(f"{number} is negative.")
@@ -13,7 +14,9 @@ elif number > 0  :
 else:              #If the number is neither greater than nor less than zero, the only number left is zero.
     print(f"{number} is zero.")
 
-#conditional block #2 tests number parity and zero case
+# #conditional block #2 tests number parity by dividing the number by two and checking if the remainder is 0 or 1.
+# #If the number does not have a remainder, the first branch of the block is true so it runs
+# #If the number has a remainder it fails the first branch the else branch will run. 
 
 if number % 2 == 0 :
     print(f"{number} is even.")
