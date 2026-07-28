b54d9ce3652a3e09105214019fe2f147ac68d67e
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-27 23:56:22 -0400

    completed warmup1

---
+++ week-4/assignment-4/warmup1.py
@@ -0,0 +1,14 @@
+#hard coded list of 8 numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+#print the first item
+print(numbers[0])
+
+#print the last item
+print(numbers[-1])
+
+#print the middle 4 numbers
+print(numbers[2:6])
+
+#print the list in reverse order
+print(list(reversed(numbers)))
