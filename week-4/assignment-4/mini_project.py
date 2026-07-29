8286895fe2e1d7e421bb855ac5933e3a96d59e70
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-07-28 21:26:40 -0400

    completed the requirements for mini_project

--- week-4/assignment-4/mini_project.py
+++ week-4/assignment-4/mini_project.py
@@ -12,22 +12,36 @@ students = [
 ]
 
 total_score = 0      # Gives a fresh starting point for the total_score variable that we can iteratively add to.
-top_score = None    #
-subjects = set()
+top_score = None    #Sets equal to None because the variable is a placeholder and has the ability to change.
+top_score_name = None #Creates a placeholder for top_score_name
+subjects = set()   #Creates a new empty set that we will loop .add to from the item in students 
+high_scorers = []
 
 for item in students : 
+    # Assigns current_score to the values using the item["key"]
     current_score = item["score"]
+    #Since we've defined total_score as a variable starting @ 0 . we accumulate the score value over each pass of item in students
     total_score = total_score + item["score"]
-    # subjects.add(students["subject"])
+
+    subjects.add(item["subject"])
 
     if top_score is None or current_score > top_score:
+
         top_score = current_score
+
+        top_score_name = item["name"]
+
+    if current_score > 75 :
+        high_scorers.append(item["name"])
+
 
 class_avg = (total_score)/(len(students))
 
+print(f"Top scorer: {top_score_name} ({top_score})")
 
+print(f"Clas average: {class_avg}") 
 
+print(f"Subjects offered: {subjects}") 
 
-print(top_score)
-print(class_avg) 
-print(subjects)      
+print(f"High scorers: {high_scorers}")
+
