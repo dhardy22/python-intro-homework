6e6cd186d4d834d6aaee5bb7219f6d6bb393e15b
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-08-18 17:23:37 -0400

    changed to the relative pathway from instructions

--- week-7/assignment-7/warmup2.py
+++ week-7/assignment-7/warmup2.py
@@ -1,6 +1,6 @@
 import csv 
 
-with open('/Users/donovanhardy/CTD/python_class/python-intro-homework/week-7/data/students.csv', 'r') as file:
+with open('../data/students.csv', 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
         print(f"{row['name']}: {row['score']}")
