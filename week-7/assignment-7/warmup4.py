c83c59a617078dc862c0cb708272527447fb16ef
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-08-18 12:28:55 -0400

    completed warmup4

---
+++ week-7/assignment-7/warmup4.py
@@ -0,0 +1,14 @@
+'''
+Print today's date in the following format:
+
+Today is April 24, 2026.
+Use datetime.now() and .strftime().
+Save as: warmup4.py
+
+'''
+
+from datetime import datetime
+
+now = datetime.now()
+
+print(f"Today is {now.strftime("%B, %d, %Y")}")
