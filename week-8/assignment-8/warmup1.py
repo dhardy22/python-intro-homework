f3563bc0fd87542214b08b9fa655dc5a368045b5
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-08-22 12:42:44 -0400

    added a while loop to check for the valid number condition

--- week-8/assignment-8/warmup1.py
+++ week-8/assignment-8/warmup1.py
@@ -1,7 +1,11 @@
-try:
-    user_num = float(input("Enter a number: "))
-except ValueError:
-    print(f"That's not a valid number. Try again.")
-else:
-    print(f"You entered: {user_num}")
+validated = False #creates a conditional variable to check if the user input is valid
+
+while not validated:
+    try:
+        user_num = float(input("Enter a number: "))
+    except ValueError:
+        print("That's not a valid number. Try again.")
+    else:
+        print(f"You entered: {user_num}")
+        validated = True #when the input is valid, the conditional variable is set to True and the loop ends
 
