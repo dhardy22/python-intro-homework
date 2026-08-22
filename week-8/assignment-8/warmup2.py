04a45ccc3121307ec069f70d4832666a6ca8cbbd
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-08-22 14:09:22 -0400

    moved inputs to the try block and added valueerror handling

--- week-8/assignment-8/warmup2.py
+++ week-8/assignment-8/warmup2.py
@@ -1,11 +1,12 @@
-
-numerator = float(input("Enter the numerator: "))
-denominator = float(input("Enter the denominator: "))
-
+#script to divide two numbers with exception handling
 try:
+    numerator = float(input("Enter the numerator: "))
+    denominator = float(input("Enter the denominator: "))
     result = numerator / denominator
 except ZeroDivisionError:
     print("Can't divide by zero — please try a non-zero denominator.")
+except ValueError:
+    print("Please enter valid numbers")
 else:
    print(f"{numerator} ÷ {denominator} = {result}")
 
