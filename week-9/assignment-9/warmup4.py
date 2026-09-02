149d03821a78906241c8fbbca39f95fb4c82c18e
dhardy22 <102342889+dhardy22@users.noreply.github.com>
2026-09-01 22:55:52 -0400

    updated the error message

--- week-9/assignment-9/warmup4.py
+++ week-9/assignment-9/warmup4.py
@@ -6,6 +6,7 @@ try:
     r = requests.get(
         url
     )
+    print("Success!")
 except requests.exceptions.RequestException as e:
     print("Error: Could not reach the server. Check your connection and try again:")
     
